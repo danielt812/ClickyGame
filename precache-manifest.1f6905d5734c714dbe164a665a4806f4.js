402b04358188ecc69e27ce7c19706e802be0ba7e
Daniel Tolan <danieltolan@gmail.com>
2019-06-16 02:15:15 -0500

    Updates

---
+++ precache-manifest.1f6905d5734c714dbe164a665a4806f4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d057a94dd3c8f03f78439531d9651cd5",
+    "url": "/RnM_MemoryGame/index.html"
+  },
+  {
+    "revision": "8547613e958b9d8fa142",
+    "url": "/RnM_MemoryGame/static/css/main.cdf5e21b.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/RnM_MemoryGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "8547613e958b9d8fa142",
+    "url": "/RnM_MemoryGame/static/js/main.567dcab4.chunk.js"
+  },
+  {
+    "revision": "0c30e2070b34ec89d74b",
+    "url": "/RnM_MemoryGame/static/js/runtime~main.fdafaea3.js"
+  },
+  {
+    "revision": "0d533a8327df5569e02aba4fd3497461",
+    "url": "/RnM_MemoryGame/static/media/background.0d533a83.jpg"
+  },
+  {
+    "revision": "4b4420cffe3ba4f13221e529e5b60078",
+    "url": "/RnM_MemoryGame/static/media/getSchwifty.4b4420cf.ttf"
+  },
+  {
+    "revision": "4adf57356a5bb50c8b48f328ac1f1e32",
+    "url": "/RnM_MemoryGame/static/media/handwriting.4adf5735.TTF"
+  },
+  {
+    "revision": "c8f1fe23d59541f09ca9657643eebc63",
+    "url": "/RnM_MemoryGame/static/media/morty.c8f1fe23.ttf"
+  },
+  {
+    "revision": "1801a3694b1faff4a369a310bc698cc1",
+    "url": "/RnM_MemoryGame/static/media/rick.1801a369.ttf"
+  }
+]);
